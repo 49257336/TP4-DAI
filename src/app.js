4c6114f5a77657de8a0a0bcbadc0ec65ee872c28
MILENA WINER <49257336@ort>
2026-04-20 09:15:45 -0300

    Se arreglo el index.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import {devolverHora, fecha} from "./time.js";
 
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
     res.send("Bienvenido/a");
@@ -16,6 +15,5 @@ app.get('/fecha-completa', (req, res) => {
     res.send(`${fecha()}`);
 })
 
-app.listen(port, () => {
-    console.log("escuchando el puerto "+port);
-});
+export default app;
+
